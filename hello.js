b4925ffae5aac28e1dc69b9baf6c73ef2b8d0c90
Rachel <racheljune2004@gmail.com>
2022-09-25 00:01:15 -0700

    code so far

---
+++ hello.js
@@ -0,0 +1,29 @@
+const slide1 = [1];
+const slide2 = [2];
+const slide3 = [3];
+const slide4 = [4];
+const slide5 = [5];
+const slide6 = [6];
+const slide7 = [7];
+const slide8 = [8];
+const slide9 = [9];
+const slide10 = [10];
+const slides = [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10];
+console.log(slides);
+
+// adding topic to slide
+// slides
+let showSlides = []
+for (let i = 0; i < 10; i++) {
+    let checkSlide = slides[i];
+    if (checkSlide.includes("cells")) {
+        showSlides.push(slides[i]);
+    }
+}
+console.log(showSlides);
+console.log(slides[1]);
+console.log(slides);
+//LO1 = [2,4,6]
+
+// adding learning objective to slide
+// adding other filters to slide
